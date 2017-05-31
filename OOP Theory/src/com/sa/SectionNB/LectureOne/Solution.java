286af5cff87a5023abd442b5bf3358917564aff5
MD. MOFIJUL ISLAM AKASH <akash.cse.du@gmail.com>
2017-05-31 16:50:48 +0600

    upto Wednesday 31 May 2017

--- OOP Theory/src/com/sa/SectionNB/LectureOne/Solution.java
+++ OOP Theory/src/com/sa/SectionNB/LectureOne/Solution.java
@@ -1,5 +1,7 @@
 package com.sa.SectionNB.LectureOne;
 
+import java.util.Scanner;
+
 /**
  * Created by akashs on 5/29/17.
  */
@@ -15,7 +17,47 @@ public class Solution {
 
         a=10;
 
+        if(a%2==0){
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
+
+        int[] ar = new int[5];
+        int[][] ar2 = new int[5][10];
+        int[][] ad2 = new int[5][];
+
+        ad2[0] = new int[3];
+        ad2[1] = new int[2];
+        ad2[2] = new int[5];
+
+
+        for(int i=0;i<5;i++){
+            ar[i] = 10+i;
+        }
+        for(int i=0;i<5;i++){
+            System.out.println(ar[i]);
+        }
+        System.out.println("For each loop print");
+        for(int num : ar){
+            System.out.println(num);
+        }
+        System.out.println(ar.length);
+
         System.out.println("A = "+a);
+
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.nextInt();
+        d = sc.nextDouble();
+
+        String s;
+        s = sc.next();
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(s);
+
 
 
     }
